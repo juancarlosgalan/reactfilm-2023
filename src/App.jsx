b8f0e9d42e6432abac43ecd8f9018c9eb1e92352
JuanCarlosGalan <juancgalan47@gmail.com>
2023-11-03 14:14:17 -0300

    [cfg] deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 const App = () => {
-  return <div>
-    <h1>APP PARA PELIS</h1>
-  </div>
+  return (
+    <div>
+      <h1>APP PARA PELIS</h1>
+      <h2>PRUEBA DEPLOY</h2>
+    </div>
+  );
 };
 
 export default App;
